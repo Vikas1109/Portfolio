f5d92a8e89660cd088f91a328581b27160cf4da8
Vikas Chauhan <vikaschauhan1109@gmail.com>
2022-09-25 18:10:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// type writer
+
+var typed = new Typed(".auto-type", {
+  strings: ["Vikas Chauhan", "Software Developer", "Coder", "Designer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
